a4f977307d7e7e3631672032ae21fa7fe08477b7
Todd Alloway <kinome79@yahoo.com>
2023-04-05 20:16:51 -0400

    feat: added some certification details

--- res-scripts/data.js
+++ res-scripts/data.js
@@ -1,30 +1,30 @@
 const certInfo = {
     0 : {"title": "Harvard CS50 Web Programming Course",
-        "desc":"Description",
+        "desc":"A CS50 web development course, covering web technologies including HTML and CSS, Git, Python, Django, SQL management, and Javascript. Certification ivolved submitting 4 focused projects as well as a Final Project",
         "url": "./res-images/certifications/CS50W Course Cert.JPG"},
     
-    1 : {"title": "Title",
-        "desc":"Description",
+    1 : {"title": "Responsive Web Design",
+        "desc":"An fCC course covering web design with HTML and CSS, visual design, accessibility, responsive media queries, Flexbox layouts, and more. Certification required completing 5 projects covering skills learned",
         "url": "./res-images/certifications/Responsive Web Design Cert.JPG"},
 
-    2 : {"title": "Title",
-        "desc":"Description",
+    2 : {"title": "Front End Libraries",
+        "desc":"An fCC course introducing various popular front-end libraries, including BootStrap, FontAwesome, jQuery, SASS, React paired with Redux, and more. Certification required completing 5 projects covering skills rturned",
         "url": "./res-images/certifications/Front End Developement Libraries.JPG"},
     
-    3 : {"title": "Title",
-        "desc":"Description",
+    3 : {"title": "JavaScript Algorithms and Data Structures",
+        "desc":"An fCC course covering Javascript, covering basic algorithms, data structures, Objects, new ES6 implementations, RegEx, and its integration with Web Design . Certification required completing 5 scripting projects",
         "url": "./res-images/certifications/JavaScript Algorithms and Data Structures.JPG"},
 
-    4 : {"title": "Title",
-        "desc":"Description",
+    4 : {"title": "Back End Development and APIs",
+        "desc":"An fCC course covering backend web development and API usage, particularly with Node.js utilizing NPM, Express, MongoDB with Mongoose. Certification required completion of 5 backend microservice projects",
         "url": "./res-images/certifications/Back End Development and APIs cert.JPG"},
     
-    5 : {"title": "Title",
-        "desc":"Description",
+    5 : {"title": "Quality Assurance",
+        "desc":"An fCC course covering testing practices with Chai/Mocha, advances Node.js Sockets, OAuth and Passport session authentication, middleware, and more. Certification required completion of 5 Node projects.",
         "url": "./res-images/certifications/Quality Assurance Cert.JPG"},
         
-    6 : {"title": "Title",
-        "desc":"Description",
+    6 : {"title": "Relational Database Scripting",
+        "desc":"An fCC course covering SQL with Postgres, table manipulations, queries, data generation, and manipulation with bash Scripting in Linux. Certification required creation ",
         "url": "./res-images/certifications/Relational Database Cert.JPG"},
     
     7 : {"title": "Title",
